3c10b074645d50110149b24d4d75f6e14c8a1889
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-05-04 12:23:55 -0400

    updated aboutme

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,7 +11,7 @@ function AboutMe() {
         <p className="">
         Through my most recent course I have learned from the ground up how to create my own PWA’s and single page web applications. I’ve made my own servers using Express and Node.js that I’ve used to connect the databases that I have made using MySQL. I’ve created my own RESTful APIs, and communicated with outside web APIs to make fast web apps that dynamically display a variety of information.</p>
         <p>
-        The start of my career has been working in hospitality where I have learned how to adapt to any problem quickly under pressure, have clear concise communication skills, work well as a team, and meet goals in an extremely fast paced environment under high stress. I have worked my way up to management at every job I have had due to an organized and thorough work ethic. If you have a look at some of my work history in my resume section of this application you will see that I have maintained full time employment in higher ranking positions throughout my time in this field. </p>
+        The start of my career has been working in hospitality where I have learned how to adapt to any problem quickly under pressure, have clear concise communication skills, work well as a team, and meet goals in an extremely fast paced environment under high stress. I have worked my way up to management at every job I have had due to an organized and thorough work ethic. If you have a look at some of my work history in my resume section of this webapp you will see that I have maintained full time employment in higher ranking positions throughout my time in this field. </p>
         </div>
         </div>
     )
