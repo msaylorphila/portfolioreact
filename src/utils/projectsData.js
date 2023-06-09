e5222589a2ad3949c2dc81f93c3c67a043658fb3
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-06-09 10:25:22 -0400

    updated projects data

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -10,46 +10,47 @@ export const projectsData = [
     },
     {
       id: 2,
+      title: "Brew Buddies",
+      image: require("../images/Brew-Buddies.png"),
+      githubLink: "https://github.com/miacias/brew-buddies",
+      deployedLink: "https://fierce-basin-42875.herokuapp.com/",
+    },
+    {
+      id: 3,
       title: "Horiseon",
       image: require("../images/horiseon-screenshot.png"),
       githubLink: "https://github.com/msaylorphila/msaylor-accessibility-mod1",
       deployedLink: "https://msaylorphila.github.io/msaylor-accessibility-mod1/",
     },
     {
-      id: 3,
+      id: 4,
       title: "Model-View-Controller Techblog",
       image: require("../images/mvp-screenshot.png"),
       githubLink: "https://github.com/msaylorphila/model-view-controller-techblog",
       deployedLink: "https://mvp-techblog.herokuapp.com/",
     },
     {
-      id: 4,
+      id: 5,
       title: "Vilca Events",
       image: require("../images/vilca-screenshot.png"),
       githubLink: "https://github.com/msaylorphila/Vilca--Events",
       deployedLink: "https://glacial-escarpment-38096.herokuapp.com/",
     },
     {
-      id: 5,
+      id: 6,
       title: "Workday Planner",
       image: require("../images/workday-screenshot.png"),
       githubLink: "https://github.com/msaylorphila/msaylor-dailyplanner-mod05",
       deployedLink: "https://msaylorphila.github.io/msaylor-dailyplanner-mod05/",
     },
     {
-      id: 6,
+      id: 7,
       title: "Weather Dashboard",
       image: require("../images/weather-screenshot.png"),
       githubLink: "https://github.com/msaylorphila/msaylor-weatherapp-mod06",
       deployedLink: "https://msaylorphila.github.io/msaylor-weatherapp-mod06/",
     },
-    {
-      id: 6,
-      title: "Weather Dashboard",
-      image: require("../images/Brew-Buddies.png"),
-      githubLink: "https://github.com/miacias/brew-buddies",
-      deployedLink: "https://fierce-basin-42875.herokuapp.com/",
-    },
+   
   ];
 
   
